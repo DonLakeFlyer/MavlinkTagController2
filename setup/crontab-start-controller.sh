574c15e9d1b4b600c171fd9d5a948087b45f0ed1
Don Gagne <don@thegagnes.com>
2026-02-26 09:33:13 -0800

    Fix crontab script for new controller build location

--- setup/crontab-start-controller.sh
+++ setup/crontab-start-controller.sh
@@ -4,4 +4,4 @@ if [ -f /home/pi/MavlinkTagController.log ]; then
     tail -n 500 /home/pi/MavlinkTagController.log.save >> /home/pi/MavlinkTagController.log
     rm /home/pi/MavlinkTagController.log.save
 fi
-/home/pi/repos/MavlinkTagController2/build/MavlinkTagController2 serial:///dev/serial0:921600 >>/home/pi/MavlinkTagController.log 2>&1
+/home/pi/repos/MavlinkTagController2/build/controller/MavlinkTagController2 serial:///dev/serial0:921600 >>/home/pi/MavlinkTagController.log 2>&1
